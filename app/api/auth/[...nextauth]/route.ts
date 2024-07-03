02012d13153ccbc866d59674ffb12aba35abcfb8
nidalA1992 <nidalakhmedjanov@gmail.com>
2024-07-03 23:19:36 +0500

    add next auth api/route

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,64 @@
+import bcrypt from 'bcrypt';
+import NextAuth, { AuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
+import prisma from '@/app/libs/prismadb';
+import { InvalidCredentialsException } from '@/app/errors/InvalidCredentialsException';
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials.password) {
+          throw new InvalidCredentialsException();
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email
+          }
+        });
+
+        if (!user || !user.hashedPassword) {
+          throw new InvalidCredentialsException()
+        }
+
+        const isCorrectPassord = await bcrypt.compare(
+          credentials.password, user.hashedPassword
+        );
+
+        if (!isCorrectPassord) {
+          throw new InvalidCredentialsException();
+        }
+
+        return user;
+      },
+    })
+  ],
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: 'jwt'
+  },
+  secret: process.env.NEXTAUTH_SECRET
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
