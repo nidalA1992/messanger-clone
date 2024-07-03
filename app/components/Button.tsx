1899a7be3a664cfd944b6b89090ad8d638be8714
nidalA1992 <nidalakhmedjanov@gmail.com>
2024-07-03 23:18:09 +0500

    button and input components

---
+++ app/components/Button.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import clsx from 'clsx';
+
+interface IButtonProps {
+  type?: 'button' | 'submit' | 'reset' | undefined;
+  fullWidth?: boolean;
+  children?: React.ReactNode;
+  onClick?: VoidFunction;
+  secondary?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+}
+
+function Button({
+  type,
+  fullWidth,
+  children,
+  onClick,
+  secondary,
+  danger,
+  disabled,
+}: IButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+      className={clsx(
+        `
+        flex
+        justify-center
+        rounded-md
+        px-3
+        py-2
+        text-sm
+        font-semibold
+        focus-visible:outline
+        focus-visible:outline-2
+        focus-visible:outline-offset-2
+      `,
+        disabled && 'opacity-50 cursor-default',
+        fullWidth && 'w-full',
+        secondary ? 'text-gray-900' : 'text-white',
+        danger &&
+          'bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600',
+        !secondary &&
+          !danger &&
+          'bg-sky-600 focus-visible:outline-sky-600',
+      )}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
